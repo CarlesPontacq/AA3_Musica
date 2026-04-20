938becf8656a599e6a2b59efeda78a24c7f207f9
CarlesPontacq <carles.pontacq@enti.cat>
2026-04-20 11:53:53 +0200

    Added Lucidity project

---
+++ Lucidity/Assets/Scripts/Anomalies/Act1/Thunderstorm/LightningController.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class LightningController : MonoBehaviour
+{
+    [Header("Lightning")]
+
+    [SerializeField] private GameObject[] lightnings;
+
+    [Range(1f, 2f)]
+    [SerializeField] private float firstLightnintTimer = 1.7f;
+
+    public float lightningMinDuration = 0.125f;
+    public float lightningMaxDuration = 0.75f;
+
+    public float nextLightningMinWaiting = 3.5f;
+    public float nextLightningMaxWaiting = 7.7f;
+
+    [Header("Thunder")]
+    [SerializeField] private string audioName = "Thunders";
+    [SerializeField] private float audioVolume = 1f;
+
+    [SerializeField] private float thunderMinDelay = 0.5f;
+    [SerializeField] private float thunderMaxDelay = 3.5f;
+
+    void Start()
+    {
+        foreach (var l in lightnings)
+            l.SetActive(false);
+
+        Invoke(nameof(CallLightning), firstLightnintTimer);
+    }
+
+    void CallLightning()
+    {
+        int randomLightning = Random.Range(0, lightnings.Length);
+        float lightningDuration = Random.Range(lightningMinDuration, lightningMaxDuration);
+
+        lightnings[randomLightning].SetActive(true);
+        Invoke(nameof(EndLightning), lightningDuration);
+
+        CallThunderWithDelay();
+
+        float rand = Random.Range(nextLightningMinWaiting, nextLightningMaxWaiting);
+        Invoke(nameof(CallLightning), rand);
+    }
+
+    void EndLightning()
+    {
+        foreach (var l in lightnings)
+            l.SetActive(false);
+    }
+
+    private void CallThunderWithDelay()
+    {
+        if (SFXManager.Instance == null) return;
+
+        float delay = Random.Range(thunderMinDelay, thunderMaxDelay);
+        Invoke(nameof(PlayThunder), delay);
+    }
+
+    private void PlayThunder()
+    {
+        if (SFXManager.Instance != null)
+            SFXManager.Instance.PlayGlobalSound(audioName, audioVolume);
+    }
+}
